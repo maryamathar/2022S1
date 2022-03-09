eb91bb766e7e4813e3168c247f914c9045eaa2c2
maryamathar <maryam.athar@students.mq.edu.au>
2022-03-09 18:51:18 +1100

    Update a1.py

--- assignments/a1/a1.py
+++ assignments/a1/a1.py
@@ -5,18 +5,11 @@ nltk.download('punkt')
 nltk.download('gutenberg')
 from sklearn.feature_extraction.text import TfidfVectorizer
 import collections
+nltk.download('stopwords')
 
 # Task 1 (2 marks)
 def count_pos(document, pos):
-    nltk.download("universal_tagset")
-    for document in nltk.corpus.gutenberg.fileids():
-        print(document)
-    dcounter = collections.Counter(nltk.corpus.gutenberg.words(document))
 
-    text = word_tokenize(dcounter)
-    count = nltk.pos_tag(text, tagset = "universal" )
-
-    
     """Return the number of occurrences of words with a given part of speech. To find the part of speech, use 
     NLTK's "Universal" tag set. To find the words of the document, use NLTK's sent_tokenize and word_tokenize.
     >>> count_pos('austen-emma.txt', 'NOUN')
@@ -24,7 +17,17 @@ def count_pos(document, pos):
     >>> count_pos('austen-sense.txt', 'VERB')
     25074
     """
-    return count
+    nltk.download('averaged_perceptron_tagger')
+    import collections
+    sents = [nltk.word_tokenize(s) for s in nltk.sent_tokenize(nltk.corpus.gutenberg.raw(document))]
+    tagged_sents = nltk.pos_tag_sents(sents, tagset="universal")
+    pos1 = []
+    for s in tagged_sents:
+        for w in s:
+            pos1.append(w[1])
+    counter = collections.Counter(pos1)
+    
+    return counter.get(pos)
 
 # Task 2 (2 marks)
 def get_top_stem_bigrams(document, n):
@@ -36,7 +39,14 @@ def get_top_stem_bigrams(document, n):
     >>> get_top_stem_bigrams('austen-sense.txt',4)
     [(',', 'and'), ('.', "''"), (';', 'and'), (',', "''")]
     """
-    return []
+    sent_tokens = [word_tokenize(s) for s in sent_tokenize(nltk.corpus.gutenberg.raw(document))]
+    bigrams = []
+    for s in sent_tokens:
+        bigrams += nltk.bigrams(s)
+    c = collections.Counter(bigrams)
+    #print(c.most_common(n))
+    return [b for b, f in c.most_common(n)]
+
 
 
 # Task 3 (2 marks)
@@ -61,10 +71,19 @@ def most_frequent_after_pos(document, pos):
     >>> most_frequent_after_pos('austen-sense.txt','NOUN')
     [(',', 5310)]
     """
-    return []
+    sent_tokens = [word_tokenize(s) for s in sent_tokenize(nltk.corpus.gutenberg.raw(document))]
+    sent_pos = nltk.pos_tag_sents(sent_tokens, tagset='universal')
+    
+    filtered_pos = []
+    for s in sent_pos:
+        bigrams = nltk.bigrams(s)
+        filtered_pos += [w2 for (w1, p1), (w2, p2) in bigrams if p1 == pos]
+    c = collections.Counter(filtered_pos)
+    return c.most_common(1)
 
 # Task 5 (2 marks)
 def get_word_tfidf(text):
+
     """Return the tf.idf of the words given in the text. If a word does not have tf.idf information or is zero, 
     then do not return its tf.idf. The reference for computing tf.idf is the list of documents from the NLTK 
     Gutenberg corpus. To compute the tf.idf, use sklearn's TfidfVectorizer with the option to remove the English 
@@ -75,7 +94,16 @@ def get_word_tfidf(text):
     >>> get_word_tfidf('Brutus is a honourable person')
     [('brutus', 0.8405129362379974), ('honourable', 0.4310718596448824), ('person', 0.32819971943754456)]
     """
-    return []
+    tfidf = TfidfVectorizer(input='content',stop_words='english')
+    data = [nltk.corpus.gutenberg.raw(f) for f in nltk.corpus.gutenberg.fileids()]
+    tfidf.fit(data)
+    result = tfidf.transform([nltk.corpus.gutenberg.raw(text)]).toarray()
+    words = tfidf.get_feature_names()
+    sorted_words = sorted(words, key=lambda x: result[0, words.index(x)], reverse=True)
+    #print('thee:', result[0, words.index('thee')])
+    #print('thel:', result[0, words.index('thel')])
+    print (sorted_words)
+    return sorted_words
 
 
 # DO NOT MODIFY THE CODE BELOW
